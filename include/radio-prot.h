4e385a9443c2c1590f328590893eb96805e6d8da
Sean Cross <sean@xobs.io>
2017-06-13 16:07:13 +0800

    radio-prot: add firmware protocols
    Signed-off-by: Sean Cross <sean@xobs.io>

--- include/radio-prot.h
+++ include/radio-prot.h
@@ -2,12 +2,14 @@
 #define RADIO_PROT_H
 
 enum radio_protocols {
-  radio_prot_paging         = 1,
-  radio_prot_dhcp_request   = 2,
-  radio_prot_dhcp_response  = 3,
-  radio_prot_echo           = 4,
-  radio_prot_dut_to_peer    = 6,
-  radio_prot_peer_to_dut    = 7,
+  radio_prot_paging             = 1,
+  radio_prot_dhcp_request       = 2,
+  radio_prot_dhcp_response      = 3,
+  radio_prot_echo               = 4,
+  radio_prot_dut_to_peer        = 6,
+  radio_prot_peer_to_dut        = 7,
+  radio_prot_request_firmware   = 8,
+  radio_prot_firmware_data      = 9,
 };
 
 #endif /* RADIO_PROT_H */
