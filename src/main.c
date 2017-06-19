2917c21692943d83688bf5cb245c0ab3da7dd44d
Sean Cross <sean@xobs.io>
2017-06-19 10:57:13 +0800

    main: handle palawan_tx board
    An initial setup for the palawan_tx board.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/main.c
+++ src/main.c
@@ -1,74 +1,56 @@
-#include <stdint.h>
+#include "dhcp-server.h"
 #include "kl17.h"
-#include "spi.h"
 #include "palawan.h"
 #include "radio.h"
-#include "dhcp-server.h"
+#include "spi.h"
+#include <stdint.h>
 
 extern void usbStart(void);
 void usbProcess(void (*received_data)(void *data, uint32_t size));
 void echoServerSetup(KRadioDevice *radio);
 void firmwareServerSetup(KRadioDevice *radio);
+void aboutServerSetup(KRadioDevice *radio);
 
-static void received_data(void *data, uint32_t bytes)
-{
+static void received_data(void *data, uint32_t bytes) {
   (void)data;
   (void)bytes;
   FGPIOB->PTOR = (1 << 1);
 }
 
-static void configure_led(void)
-{
+static void configure_led(void) {
   PORTB->PCR[1] = (1 << 8) | (1 << 2);
   FGPIOB->PDDR |= (1 << 1);
   FGPIOB->PSOR = (1 << 1);
 }
 
-__attribute__((noreturn)) void main(void)
-{
+__attribute__((noreturn)) void palawan_main(void) {
   configure_led();
   spiInit();
   radioInit();
 
   radioStart(radioDevice);
-  if (palawanModel() == palawan_rx) {
+  switch (palawanModel()) {
+  case palawan_rx:
     radioSetAddress(radioDevice, 0);
     dhcpServerSetup(radioDevice);
     echoServerSetup(radioDevice);
+    aboutServerSetup(radioDevice);
     firmwareServerSetup(radioDevice);
-  }
+    usbStart();
 
-  usbStart();
-  radioDumpFifo();
-  radioDumpData(1, 1);
-
-  while (1) {
-    extern uint8_t packetAvailable;
-    usbProcess(received_data);
-    if (packetAvailable) {
-      packetAvailable = 0;
-      radioPoll(radioDevice);
-    }
-  #if 0
-    else {
-      static uint8_t bfr[32];
-      static int loops = 0;
-      unsigned int i;
-      for (i = 0; i < sizeof(bfr); i+=2) {
-        bfr[i] = 0x55;
-        bfr[i+1] = 0xaa;
+    while (1) {
+      extern uint8_t packetAvailable;
+      usbProcess(received_data);
+      if (packetAvailable) {
+        packetAvailable = 0;
+        radioPoll(radioDevice);
       }
-      loops++;
-      static const char hex_digits[] = "0123456789abcdef";
-      bfr[0] = hex_digits[(loops>>12) & 0xf];
-      bfr[1] = hex_digits[(loops>>8) & 0xf];
-      bfr[2] = hex_digits[(loops>>4) & 0xf];
-      bfr[3] = hex_digits[(loops>>0) & 0xf];
-      bfr[4] = ' ';
-      bfr[31] = '\0';
-      radioSend(radioDevice, 0xff, radio_prot_echo, sizeof(bfr), bfr);
     }
-#endif
+  case palawan_tx:
+    break;
+  default:
+    asm("bkpt #63");
   }
-
+  while (1)
+    ;
 }
