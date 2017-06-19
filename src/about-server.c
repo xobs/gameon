b85e31cc62294e3c54d3ea92b3ceab5e63eec10e
Sean Cross <sean@xobs.io>
2017-06-19 18:20:33 +0800

    about-server: use global gitver
    Don't bake it into the source file.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/about-server.c
+++ src/about-server.c
@@ -1,4 +1,5 @@
 #include "about.h"
+#include "gitversion.h"
 #include "kl17.h"
 #include "radio.h"
 #include <stdint.h>
@@ -12,7 +13,7 @@ static void about_request(uint8_t port, uint8_t src, uint8_t dst,
   (void)data;
 
   struct about_response response = {};
-  strncpy((char *)response.gitver, VERSION, sizeof(response.gitver) - 1);
+  strncpy((char *)response.gitver, git_version, sizeof(response.gitver) - 1);
 
   radioSend(radioDevice, src, radio_prot_about_response, sizeof(response),
             &response);
