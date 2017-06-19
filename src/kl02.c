817d65b7e9a8b2602e0816444639eeeb511d6f07
Sean Cross <sean@xobs.io>
2017-06-19 10:50:33 +0800

    kl02: get board model from boot token
    The boot token should already be able to indicate what sort of board
    we're running on.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/kl02.c
+++ src/kl02.c
@@ -22,8 +22,6 @@
 #define KINETIS_MCG_FLL_OUTDIV4 2 /* Divide OUTDIV1 output by 2 => 24 MHz */
 #define KINETIS_SYSCLK_FREQUENCY 47972352UL /* 32.768 kHz * 1464 (~48 MHz) */
 
-static enum palawan_model _model;
-
 /**
  * @brief   KL2x clocks and PLL initialization.
  * @note    All the involved constants come from the file @p board.h.
@@ -49,9 +47,6 @@ void kl02_clk_init(void) {
      C4[DMX32]=0 and C4[DRST_DRS]=00  =>  FLL factor=640.
      C3[SCTRIM] and C4[SCFTRIM] factory trim values apply to f_int. */
 
-  /* System oscillator drives 32 kHz clock (OSC32KSEL=0) */
-  //  SIM->SOPT1 &= ~SIM_SOPT1_OSC32KSEL_MASK;
-
   /*
    * FLL Enabled External (FEE) MCG Mode
    * 24 MHz core, 12 MHz bus - using 32.768 kHz crystal with FLL.
@@ -73,11 +68,6 @@ void kl02_clk_init(void) {
    * OUTDIV4 (additional divider for bus/flash clock) */
   SIM->CLKDIV1 = SIM_CLKDIV1_OUTDIV1(KINETIS_MCG_FLL_OUTDIV1 - 1) |
                  SIM_CLKDIV1_OUTDIV4(KINETIS_MCG_FLL_OUTDIV4 - 1);
-
-  /* EXTAL0 and XTAL0 */
-  //  PORTA->PCR[18] &= ~0x01000700; /* Set PA18 to analog (default) */  //
-  //  defaults should already be good
-  //  PORTA->PCR[19] &= ~0x01000700; /* Set PA19 to analog (default) */
 
   OSC0->CR = 0xC;
 
@@ -118,117 +108,7 @@ void kl02_clk_init(void) {
 
 enum palawan_model palawanModel(void) {
   /* The strapping resistors were wired up to a pin that can't do ADC */
-  return palawan_rx;
-#if 0
-  /* Sample the strapping resistors, to determine model type */
-  if ((_model != palawan_tx) && (_model != palawan_rx)) {
-    uint16_t gain;
-    int resistance;
-    int resistance_min;
-    int resistance_max;
-
-    /* Ungate PORTE, where the ADC we're using lives */
-    SIM->SCGC5 |= SIM_SCGC5_PORTB;
-
-    /* Configure PTE30 to be an input pad */
-    PORTB->PCR[12] = PORTx_PCRn_MUX(0);
-
-    /* Ungate the ADC */
-    SIM->SCGC6 |= SIM_SCGC6_ADC0;
-
-    /* Quick-and-dirty calibration */
-    ADC0->CFG1 =  ADCx_CFG1_ADIV(ADCx_CFG1_ADIV_DIV_8) |
-                  ADCx_CFG1_ADICLK(ADCx_CFG1_ADIVCLK_BUS_CLOCK_DIV_2);
-
-    /* Software trigger, no DMA */
-    ADC0->SC2 = 0;
-
-    /* Enable hardware averaging over 32 samples, and run calibration */
-    ADC0->SC3 = ADCx_SC3_AVGE |
-                ADCx_SC3_AVGS(ADCx_SC3_AVGS_AVERAGE_32_SAMPLES) |
-                ADCx_SC3_CAL;
-
-    /* Wait for calibration to finish */
-    while (!(ADC0->SC1A & ADCx_SC1n_COCO))
-      ;
-
-    /* Adjust gain according to reference manual */
-
-    gain = ((ADC0->CLP0 + ADC0->CLP1 + ADC0->CLP2 +
-             ADC0->CLP3 + ADC0->CLP4 + ADC0->CLPS) / 2) | 0x8000;
-    ADC0->PG = gain;
-
-    gain = ((ADC0->CLM0 + ADC0->CLM1 + ADC0->CLM2 +
-             ADC0->CLM3 + ADC0->CLM4 + ADC0->CLMS) / 2) | 0x8000;
-    ADC0->MG = gain;
-
-    /* Reset Rn */
-    (void)ADC0->RA;
-
-    /* Configure for 16-bit conversion */
-    ADC0->CFG1 =  ADCx_CFG1_ADIV(ADCx_CFG1_ADIV_DIV_8) |
-                  ADCx_CFG1_ADICLK(ADCx_CFG1_ADIVCLK_BUS_CLOCK_DIV_2) |
-                  ADCx_CFG1_MODE(ADCx_CFG1_MODE_16_BITS);
-
-    /* Perform the sample read, averaging over 32 samples */
-    ADC0->SC3 = ADCx_SC3_AVGE |
-                ADCx_SC3_AVGS(ADCx_SC3_AVGS_AVERAGE_32_SAMPLES);
-
-    /* Begin the read */
-    ADC0->SC1A = ADCx_SC1n_ADCH(PAIR_CFG_ADC_NUM);
-
-    /* Wait for sample to finish */
-    while (!(ADC0->SC1A & ADCx_SC1n_COCO))
-      ;
-
-    resistance = ADC0->RA;
-
-    /* Figure out which threshold it falls into */
-    resistance_min = resistance - (PALAWAN_CFG_RESISTANCE_THRESH / 2);
-    resistance_max = resistance + (PALAWAN_CFG_RESISTANCE_THRESH / 2);
-
-    if ((resistance_min < PALAWAN_RX_VALUE)
-     && (resistance_max > PALAWAN_RX_VALUE))
-      _model = palawan_rx;
-    else if ((resistance_min < PALAWAN_RX_PAIR_VALUE)
-          && (resistance_max > PALAWAN_RX_PAIR_VALUE))
-      _model = palawan_rx;
-
-    else if ((resistance_min < PALAWAN_TX_VALUE_1)
-          && (resistance_max > PALAWAN_TX_VALUE_1))
-      _model = palawan_tx;
-
-    else if ((resistance_min < PALAWAN_TX_VALUE_2)
-          && (resistance_max > PALAWAN_TX_VALUE_2))
-      _model = palawan_tx;
-
-    else if ((resistance_min < PALAWAN_TX_VALUE_3)
-          && (resistance_max > PALAWAN_TX_VALUE_3))
-      _model = palawan_tx;
-
-    else if ((resistance_min < PALAWAN_TX_VALUE_4)
-          && (resistance_max > PALAWAN_TX_VALUE_4))
-      _model = palawan_tx;
-
-    else if ((resistance_min < PALAWAN_TX_VALUE_1_PAIR)
-          && (resistance_max > PALAWAN_TX_VALUE_1_PAIR))
-      _model = palawan_tx;
-
-    else if ((resistance_min < PALAWAN_TX_VALUE_2_PAIR)
-          && (resistance_max > PALAWAN_TX_VALUE_2_PAIR))
-      _model = palawan_tx;
-
-    else if ((resistance_min < PALAWAN_TX_VALUE_3_PAIR)
-          && (resistance_max > PALAWAN_TX_VALUE_3_PAIR))
-      _model = palawan_tx;
-
-    else if ((resistance_min < PALAWAN_TX_VALUE_4_PAIR)
-          && (resistance_max > PALAWAN_TX_VALUE_4_PAIR))
-      _model = palawan_tx;
-  }
-
-    return _model;
-#endif
+  return boot_token.board_model;
 }
 
 /**
