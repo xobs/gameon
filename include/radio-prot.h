12a1d782e27205bccd113771b7d1d8271c3177e1
Sean Cross <sean@xobs.io>
2017-06-19 10:42:12 +0800

    radio-prot: add definitions for 'about' protocol
    These protocol definitions allow for requesting information about the
    server.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- include/radio-prot.h
+++ include/radio-prot.h
@@ -2,14 +2,16 @@
 #define RADIO_PROT_H
 
 enum radio_protocols {
-  radio_prot_paging             = 1,
-  radio_prot_dhcp_request       = 2,
-  radio_prot_dhcp_response      = 3,
-  radio_prot_echo               = 4,
-  radio_prot_dut_to_peer        = 6,
-  radio_prot_peer_to_dut        = 7,
-  radio_prot_request_firmware   = 8,
-  radio_prot_firmware_data      = 9,
+  radio_prot_paging           = 1,
+  radio_prot_dhcp_request     = 2,
+  radio_prot_dhcp_response    = 3,
+  radio_prot_echo             = 4,
+  radio_prot_dut_to_peer      = 6,
+  radio_prot_peer_to_dut      = 7,
+  radio_prot_request_firmware = 8,
+  radio_prot_firmware_data    = 9,
+  radio_prot_about_request    = 10,
+  radio_prot_about_response   = 11,
 };
 
 #endif /* RADIO_PROT_H */
