54747ba70241605c54c90555e57e31ee361541c4
Sean Cross <sean@xobs.io>
2017-06-19 18:21:19 +0800

    palawan-tx: perform firmware update on boot
    If the board firmware is different, reboot and update.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/palawan-tx.c
+++ src/palawan-tx.c
@@ -1,4 +1,9 @@
+#include "about.h"
+#include "gitversion.h"
 #include "kl17.h"
+#include "palawan.h"
+#include "radio.h"
+#include <string.h>
 
 void palawanTxPinSetup(void) {
   PORTA->PCR[3]  = PORTx_PCRn_MUX(1) | PORTx_PCRn_PE | PORTx_PCRn_PS;
@@ -35,4 +40,67 @@ uint32_t palawanTxReadPins(void) {
            PAD_SHIFT(portb, 8, 12) | PAD_SHIFT(portb, 9, 13) |
            PAD_SHIFT(portb, 10, 14) | PAD_SHIFT(portb, 13, 15);
   return result;
+}
+
+/* Low-Power Timer interrupt */
+void VectorB0(void) {
+  LPTMR0->CSR = LPTMRx_CSR_TCF | LPTMRx_CSR_TIE | LPTMRx_CSR_TEN;
+}
+
+void palawanTxEnableTimer(void) {
+  /* Set up the low power timer */
+  SIM->SCGC5 |= SIM_SCGC5_LPTMR;
+
+  /* Reset the CSR, in case we're coming out of a warm reset */
+  LPTMR0->CSR = 0;
+
+  /* Select LPR */
+  LPTMR0->PSR = LPTMRx_PSR_PBYP | LPTMRx_PSR_PCS(1);
+  LPTMR0->CMR = 5; /* Approximate poll frequency in ms */
+  LPTMR0->CSR = LPTMRx_CSR_TIE | LPTMRx_CSR_TCF;
+
+  LPTMR0->CSR = LPTMRx_CSR_TIE | LPTMRx_CSR_TCF | LPTMRx_CSR_TEN;
+  __enable_irq();
+  NVIC_EnableIRQ(LPTMR0_IRQn);
+}
+
+static uint8_t about_requesting;
+
+static void about_response(uint8_t port, uint8_t src, uint8_t dst,
+                           uint8_t length, const void *data) {
+  (void)port;
+  (void)src;
+  (void)dst;
+  (void)length;
+
+  const struct about_response *response = data;
+  if (memcmp(git_version, response->gitver, strlen(git_version))) {
+    boot_token.magic = BOOT_MAGIC;
+    asm("bkpt #4");
+    NVIC_SystemReset();
+  }
+  about_requesting = 0;
+}
+
+void palawanTxVerifyFirmware(void) {
+  radioSetHandler(radioDevice, radio_prot_about_response, about_response);
+
+  uint32_t timeout_ms = 30;
+
+  while (1) {
+    uint32_t ms = 0;
+
+    about_requesting = 1;
+    radioSend(radioDevice, 0x00, radio_prot_about_request, 0, NULL);
+    while (about_requesting && (ms < timeout_ms)) {
+      radioPoll(radioDevice);
+      int i;
+      for (i = 0; i < 5000; i++)
+        asm("nop");
+      ms++;
+    }
+
+    if (!about_requesting)
+      break;
+  }
 }
