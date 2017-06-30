8f69c8242f960aa06bd051eed0f9f400f530dc0e
Sean Cross <sean@xobs.io>
2017-06-30 20:39:57 +0800

    about: send a hash as part of the program
    This will ensure we do a reflash even if the git rev hasn't changed.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- include/about.h
+++ include/about.h
@@ -3,7 +3,9 @@
 
 #include <stdint.h>
 
+#define FIRMWARE_SEED 0x84ff3209
 struct about_response {
+  uint32_t firmware_hash;
   uint8_t gitver[32];
 } __attribute__((packed));
 
--- src/about-server.c
+++ src/about-server.c
@@ -1,6 +1,7 @@
 #include "about.h"
 #include "gitversion.h"
 #include "kl17.h"
+#include "murmur3.h"
 #include "radio.h"
 #include <stdint.h>
 #include <string.h>
@@ -14,6 +15,11 @@ static void about_request(uint8_t port, uint8_t src, uint8_t dst,
 
   struct about_response response = {};
   strncpy((char *)response.gitver, git_version, sizeof(response.gitver) - 1);
+  extern uint32_t __app_start__;
+  extern uint32_t __app_end__;
+  MurmurHash3_x86_32(&__app_start__,
+                     ((uint32_t)&__app_end__) - ((uint32_t)&__app_start__),
+                     FIRMWARE_SEED, &response.firmware_hash);
 
   radioSend(radioDevice, src, radio_prot_about_response, sizeof(response),
             &response);
